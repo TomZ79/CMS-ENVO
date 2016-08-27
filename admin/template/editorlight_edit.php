8328499fff62626b82bb291cef561604fbc00cdf
bluesatkv <bluesatkv@gmail.com>
2016-08-27 21:30:34 +0200

    Editor light
    remove responsive filemanager from light editor

--- admin/template/editorlight_edit.php
+++ admin/template/editorlight_edit.php
@@ -1,12 +1,8 @@
 <?php if ($jkv["adv_editor"]) { ?>
-  <p><a href="../js/editor/plugins/filemanager/dialog.php?type=0&editor=mce_0&lang=eng&fldr=&field_id=htmleditorlight"
-        class="btn btn-default btn-xs ifManager"><i class="fa fa-files-o"></i></a></p>
   <div id="htmleditorlight"></div>
-  <textarea name="jak_lcontent" class="form-control hidden"
-            id="jak_editor_light"><?php echo jak_edit_safe_userpost(htmlspecialchars($JAK_FORM_DATA["content"])); ?></textarea>
+  <textarea name="jak_lcontent" class="form-control hidden" id="jak_editor_light"><?php echo jak_edit_safe_userpost(htmlspecialchars($JAK_FORM_DATA["content"])); ?></textarea>
 <?php } else { ?>
-  <textarea name="jak_lcontent" class="jakEditorLight" id="jakEditor"
-            rows="40"><?php echo jak_edit_safe_userpost($JAK_FORM_DATA["content"]); ?></textarea>
+  <textarea name="jak_lcontent" class="jakEditorLight" id="jakEditor" rows="40"><?php echo jak_edit_safe_userpost($JAK_FORM_DATA["content"]); ?></textarea>
 <?php } ?>
 
 <?php if ($jkv["adv_editor"]) { ?>
--- admin/template/editorlight_new.php
+++ admin/template/editorlight_new.php
@@ -1,9 +1,6 @@
 <?php if ($jkv["adv_editor"]) { ?>
-  <p><a href="../js/editor/plugins/filemanager/dialog.php?type=0&editor=mce_0&lang=eng&fldr=&field_id=htmleditorlight"
-        class="btn btn-default btn-xs ifManager"><i class="fa fa-files-o"></i></a></p>
   <div id="htmleditorlight"></div>
-  <textarea name="jak_lcontent" class="form-control hidden"
-            id="jak_editor_light"><?php if (isset($_REQUEST["jak_lcontent"])) echo jak_edit_safe_userpost(htmlspecialchars($_REQUEST["jak_lcontent"])); ?></textarea>
+  <textarea name="jak_lcontent" class="form-control hidden" id="jak_editor_light"><?php if (isset($_REQUEST["jak_lcontent"])) echo jak_edit_safe_userpost(htmlspecialchars($_REQUEST["jak_lcontent"])); ?></textarea>
 <?php } else { ?>
   <textarea name="jak_lcontent" class="jakEditorLight" id="jakEditor" rows="40"><?php if (isset($_REQUEST["jak_lcontent"])) echo jak_edit_safe_userpost($_REQUEST["jak_lcontent"]); ?></textarea>
 <?php } ?>
