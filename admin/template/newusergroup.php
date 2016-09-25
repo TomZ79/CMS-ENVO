03e881f0179c26a7f35e72de1294ffbb4f1cde13
bluesatkv <bluesatkv@gmail.com>
2016-09-25 12:27:03 +0200

    Section: ACP - UserGroup
    - better solution for plugin settings in Usergroup

--- admin/template/newusergroup.php
+++ admin/template/newusergroup.php
@@ -55,99 +55,126 @@ if ($errors) { ?>
   </div>
 
   <form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
-    <div class="box box-primary">
-      <div class="box-header with-border">
-        <h3 class="box-title"><?php echo $tl["title"]["t18"]; ?></h3>
-      </div><!-- /.box-header -->
-      <div class="box-body">
-        <table class="table table-striped first-column v-text-center">
-          <tr>
-            <td><?php echo $tl["user"]["u"]; ?></td>
-            <td>
-              <div class="form-group no-margin<?php if (isset($errors["e1"])) echo " has-error"; ?>">
-                <input type="text" name="jak_name" class="form-control"
-                       value="<?php if (!isset($JAK_FORM_DATA["name"]) && isset($_REQUEST["jak_name"])) {
-                         echo $_REQUEST["jak_name"];
-                       } elseif (isset($JAK_FORM_DATA["name"])) { ?><?php echo $JAK_FORM_DATA["name"];
-                       } ?>"/>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td><?php echo $tl["user"]["u6"]; ?></td>
-            <td>
-              <textarea name="jak_lcontent" class="form-control" rows="4"><?php if (isset($_REQUEST["jak_lcontent"])) echo jak_edit_safe_userpost($_REQUEST["jak_lcontent"]); ?></textarea>
-            </td>
-          </tr>
-          <tr>
-            <td><?php echo $tl["user"]["u37"]; ?></td>
-            <td>
-              <div class="radio">
-                <label class="checkbox-inline">
-                  <input type="radio" name="jak_advs" value="1"<?php if (isset($_REQUEST["jak_advs"]) && $_REQUEST["jak_advs"] == '1') { ?> checked="checked"<?php }
-                  if (isset($JAK_FORM_DATA["advsearch"]) && $JAK_FORM_DATA["advsearch"] == '1') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g18"]; ?>
-                </label>
-                <label class="checkbox-inline">
-                  <input type="radio" name="jak_advs" value="0"<?php if (isset($_REQUEST["jak_advs"]) && $_REQUEST["jak_advs"] == '0') { ?> checked="checked"<?php }
-                  if (isset($JAK_FORM_DATA["advsearch"]) && $JAK_FORM_DATA["advsearch"] == '0') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g19"]; ?>
-                </label>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td><?php echo $tl["user"]["u11"]; ?></td>
-            <td>
-              <div class="radio">
-                <label class="checkbox-inline">
-                  <input type="radio" name="jak_rate" value="1"<?php if (isset($_REQUEST["jak_rate"]) && $_REQUEST["jak_rate"] == '1') { ?> checked="checked"<?php }
-                  if (isset($JAK_FORM_DATA["canrate"]) && $JAK_FORM_DATA["canrate"] == '1') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g18"]; ?>
-                </label>
-                <label class="checkbox-inline">
-                  <input type="radio" name="jak_rate" value="0"<?php if (isset($_REQUEST["jak_rate"]) && $_REQUEST["jak_rate"] == '0') { ?> checked="checked"<?php }
-                  if (isset($JAK_FORM_DATA["canrate"]) && $JAK_FORM_DATA["canrate"] == '0') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g19"]; ?>
-                </label>
-              </div>
-            </td>
-          </tr>
-        </table>
+
+    <ul class="nav nav-tabs" id="settTab">
+      <li class="active"><a href="#general"><?php echo $tl["title"]["t4"]; ?></a></li>
+      <?php if (isset($JAK_HOOK_ADMIN_USERGROUP_EDIT)) { ?>
+        <li><a href="#plugins">Plugin settings</a></li>
+      <?php } ?>
+    </ul>
+
+    <div class="tab-content">
+      <div id="general" class="tab-pane active fade in">
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title"><?php echo $tl["title"]["t18"]; ?></h3>
+          </div><!-- /.box-header -->
+          <div class="box-body">
+            <table class="table table-striped first-column v-text-center">
+              <tr>
+                <td><?php echo $tl["user"]["u"]; ?></td>
+                <td>
+                  <div class="form-group no-margin<?php if (isset($errors["e1"])) echo " has-error"; ?>">
+                    <input type="text" name="jak_name" class="form-control"
+                           value="<?php if (!isset($JAK_FORM_DATA["name"]) && isset($_REQUEST["jak_name"])) {
+                             echo $_REQUEST["jak_name"];
+                           } elseif (isset($JAK_FORM_DATA["name"])) { ?><?php echo $JAK_FORM_DATA["name"];
+                           } ?>"/>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td><?php echo $tl["user"]["u6"]; ?></td>
+                <td>
+                  <textarea name="jak_lcontent" class="form-control" rows="4"><?php if (isset($_REQUEST["jak_lcontent"])) echo jak_edit_safe_userpost($_REQUEST["jak_lcontent"]); ?></textarea>
+                </td>
+              </tr>
+              <tr>
+                <td><?php echo $tl["user"]["u37"]; ?></td>
+                <td>
+                  <div class="radio">
+                    <label class="checkbox-inline">
+                      <input type="radio" name="jak_advs" value="1"<?php if (isset($_REQUEST["jak_advs"]) && $_REQUEST["jak_advs"] == '1') { ?> checked="checked"<?php }
+                      if (isset($JAK_FORM_DATA["advsearch"]) && $JAK_FORM_DATA["advsearch"] == '1') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g18"]; ?>
+                    </label>
+                    <label class="checkbox-inline">
+                      <input type="radio" name="jak_advs" value="0"<?php if (isset($_REQUEST["jak_advs"]) && $_REQUEST["jak_advs"] == '0') { ?> checked="checked"<?php }
+                      if (isset($JAK_FORM_DATA["advsearch"]) && $JAK_FORM_DATA["advsearch"] == '0') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g19"]; ?>
+                    </label>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td><?php echo $tl["user"]["u11"]; ?></td>
+                <td>
+                  <div class="radio">
+                    <label class="checkbox-inline">
+                      <input type="radio" name="jak_rate" value="1"<?php if (isset($_REQUEST["jak_rate"]) && $_REQUEST["jak_rate"] == '1') { ?> checked="checked"<?php }
+                      if (isset($JAK_FORM_DATA["canrate"]) && $JAK_FORM_DATA["canrate"] == '1') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g18"]; ?>
+                    </label>
+                    <label class="checkbox-inline">
+                      <input type="radio" name="jak_rate" value="0"<?php if (isset($_REQUEST["jak_rate"]) && $_REQUEST["jak_rate"] == '0') { ?> checked="checked"<?php }
+                      if (isset($JAK_FORM_DATA["canrate"]) && $JAK_FORM_DATA["canrate"] == '0') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g19"]; ?>
+                    </label>
+                  </div>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div class="box-footer">
+            <button type="submit" name="save"
+                    class="btn btn-primary pull-right"><?php echo $tl["general"]["g20"]; ?></button>
+          </div>
+        </div>
+        <?php if (JAK_TAGS) { ?>
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title"><?php echo $tl["user"]["u40"]; ?></h3>
+            </div><!-- /.box-header -->
+            <div class="box-body">
+              <table class="table table-striped">
+                <tr>
+                  <td>
+                    <div class="radio">
+                      <label class="checkbox-inline">
+                        <input type="radio" name="jak_tags" value="1"<?php if (isset($_REQUEST["jak_tags"]) && $_REQUEST["jak_tags"] == '1') { ?> checked="checked"<?php }
+                        if (isset($JAK_FORM_DATA["tags"]) && $JAK_FORM_DATA["tags"] == '1') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g18"]; ?>
+                      </label>
+                      <label class="checkbox-inline">
+                        <input type="radio" name="jak_tags" value="0"<?php if (isset($_REQUEST["jak_tags"]) && $_REQUEST["jak_tags"] == '0') { ?> checked="checked"<?php }
+                        if (isset($JAK_FORM_DATA["tags"]) && $JAK_FORM_DATA["tags"] == '0') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g19"]; ?>
+                      </label>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+            </div>
+            <div class="box-footer">
+              <button type="submit" name="save"
+                      class="btn btn-primary pull-right"><?php echo $tl["general"]["g20"]; ?></button>
+            </div>
+          </div>
+        <?php } ?>
       </div>
-      <div class="box-footer">
-        <button type="submit" name="save"
-                class="btn btn-primary pull-right"><?php echo $tl["general"]["g20"]; ?></button>
-      </div>
+
+      <?php if (isset($JAK_HOOK_ADMIN_USERGROUP)) { ?>
+        <div id="plugins" class="tab-pane fade">
+          <?php if (isset($JAK_HOOK_ADMIN_USERGROUP) && is_array($JAK_HOOK_ADMIN_USERGROUP)) foreach ($JAK_HOOK_ADMIN_USERGROUP as $hs) {
+            include_once APP_PATH . $hs['phpcode'];
+          } ?>
+        </div>
+      <?php } ?>
     </div>
-    <?php if (JAK_TAGS) { ?>
-      <div class="box box-primary">
-        <div class="box-header with-border">
-          <h3 class="box-title"><?php echo $tl["user"]["u40"]; ?></h3>
-        </div><!-- /.box-header -->
-        <div class="box-body">
-          <table class="table table-striped">
-            <tr>
-              <td>
-                <div class="radio">
-                  <label class="checkbox-inline">
-                    <input type="radio" name="jak_tags" value="1"<?php if (isset($_REQUEST["jak_tags"]) && $_REQUEST["jak_tags"] == '1') { ?> checked="checked"<?php }
-                    if (isset($JAK_FORM_DATA["tags"]) && $JAK_FORM_DATA["tags"] == '1') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g18"]; ?>
-                  </label>
-                  <label class="checkbox-inline">
-                    <input type="radio" name="jak_tags" value="0"<?php if (isset($_REQUEST["jak_tags"]) && $_REQUEST["jak_tags"] == '0') { ?> checked="checked"<?php }
-                    if (isset($JAK_FORM_DATA["tags"]) && $JAK_FORM_DATA["tags"] == '0') { ?> checked="checked"<?php } ?> /> <?php echo $tl["general"]["g19"]; ?>
-                  </label>
-                </div>
-              </td>
-            </tr>
-          </table>
-        </div>
-        <div class="box-footer">
-          <button type="submit" name="save"
-                  class="btn btn-primary pull-right"><?php echo $tl["general"]["g20"]; ?></button>
-        </div>
-      </div>
-    <?php }
-    if (isset($JAK_HOOK_ADMIN_USERGROUP) && is_array($JAK_HOOK_ADMIN_USERGROUP)) foreach ($JAK_HOOK_ADMIN_USERGROUP as $hs) {
-      include_once APP_PATH . $hs['phpcode'];
-    } ?>
+
   </form>
+
+  <script type="text/javascript">
+    $(document).ready(function () {
+      $('#settTab a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+      });
+    });
+  </script>
 
 <?php include "footer.php"; ?>
