7aba96d464c5a69382896c838aa54695ae7f3e1f
bluesatkv <bluesatkv@gmail.com>
2016-09-26 20:17:26 +0200

    Section: ACP
    - Ace Editor - Add resize buttons to fullscreen mode
    - Modification css admin file

--- admin/template/editor_edit.php
+++ admin/template/editor_edit.php
@@ -12,8 +12,9 @@
               <label><?php echo $tl["general"]["g103"]; ?></label>
               <select name="restorcontent" id="restorcontent" class="form-control selectpicker" data-size="5">
                 <option value="0"><?php echo $tl["general"]["g99"]; ?></option><?php foreach ($JAK_PAGE_BACKUP as $pb) { ?>
-                  <option value="<?php echo $pb['id']; ?>"><?php echo $pb['time']; ?></option><?php } ?></select><span
-                class="loader"><i class="fa fa-spinner fa-pulse"></i></span>
+                  <option value="<?php echo $pb['id']; ?>"><?php echo $pb['time']; ?></option><?php } ?>
+              </select>
+              <span class="loader"><i class="fa fa-spinner fa-pulse"></i></span>
             </div>
           </th>
         </tr>
@@ -22,15 +23,22 @@
       <tr>
         <td>
           <?php if ($jkv["adv_editor"]) { ?>
-            <p><a
-                href="../js/editor/plugins/filemanager/dialog.php?type=0&editor=mce_0&lang=eng&fldr=&field_id=htmleditor"
-                class="btn btn-default btn-xs ifManager"><i class="fa fa-files-o"></i></a></p>
-            <div id="htmleditor"></div>
-            <textarea name="jak_content" class="form-control hidden"
-                      id="jak_editor"><?php echo jak_edit_safe_userpost(htmlspecialchars($JAK_FORM_DATA["content"])); ?></textarea>
+            <div id="cover">
+              <div class="cover-header">
+                <a href="../js/editor/plugins/filemanager/dialog.php?ty e=0&editor=mce_0&lang=eng&fldr=&field_id=htmleditor" class="btn btn-primary btn-xs ifManager" title="Show Filemanager">
+                  <i class="fa fa-files-o"></i>
+                </a>
+                <a href="#" id="resizeContainer" class="btn btn-primary btn-xs" title="Resize ACE Editor">Resize ACE Container</a>
+                <a href="#" id="resizeContainerAndEditor" class="btn btn-primary btn-xs" title="Resize ACE Editor">Resize ACE Container and ACE Editor</a>
+              </div>
+              <div id="editorContainer">
+                <div id="htmleditor"></div>
+              </div>
+            </div>
+
+            <textarea name="jak_content" class="form-control hidden" id="jak_editor"><?php echo jak_edit_safe_userpost(htmlspecialchars($JAK_FORM_DATA["content"])); ?></textarea>
           <?php } else { ?>
-            <textarea name="jak_content" class="form-control jakEditor" id="jakEditor"
-                      rows="40"><?php echo jak_edit_safe_userpost($JAK_FORM_DATA["content"]); ?></textarea>
+            <textarea name="jak_content" class="form-control jakEditor" id="jakEditor" rows="40"><?php echo jak_edit_safe_userpost($JAK_FORM_DATA["content"]); ?></textarea>
           <?php } ?>
         </td>
       </tr>
@@ -40,3 +48,53 @@
     <button type="submit" name="save" class="btn btn-primary pull-right"><?php echo $tl["general"]["g20"]; ?></button>
   </div>
 </div>
+<style type="text/css">
+  #editorContainer {
+    height: 500px;
+    position: relative;
+  }
+  #htmleditor {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+  #cover.active {
+    position:fixed;
+    top:0;
+    left:0;
+    background:#f4f4f4;
+    z-index:1050;
+    width:100%;
+    height:100%;
+    padding: 40px;
+  }
+  .cover-header {
+    background:#ddd;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+</style>
+<script type="text/javascript">
+  var clicked = false;
+  var resizeFirstEditor = function (resizeEditor) {
+    var MeContainer = document.getElementById('cover');
+    var feContainer = document.getElementById('editorContainer');
+
+    MeContainer.classList.toggle("active");
+    clicked = !clicked;
+    if(resizeEditor) {
+      editor.resize();
+    }
+  };
+
+  var btn = document.getElementById('resizeContainer');
+  btn.addEventListener('click', function () {
+    resizeFirstEditor();
+  });
+  var btn = document.getElementById('resizeContainerAndEditor');
+  btn.addEventListener('click', function () {
+    resizeFirstEditor(true);
+  });
+</script>
--- admin/template/editor_new.php
+++ admin/template/editor_new.php
@@ -4,17 +4,75 @@
   </div><!-- /.box-header -->
   <div class="box-body">
     <?php if ($jkv["adv_editor"]) { ?>
-      <p><a href="../js/editor/plugins/filemanager/dialog.php?type=0&editor=mce_0&lang=eng&fldr=&field_id=htmleditor"
-            class="btn btn-default btn-xs ifManager"><i class="fa fa-files-o"></i></a></p>
-      <div id="htmleditor"></div>
-      <textarea name="jak_content" class="form-control hidden"
-                id="jak_editor"><?php if (isset($_REQUEST["jak_content"])) echo jak_edit_safe_userpost(htmlspecialchars($_REQUEST["jak_content"])); ?></textarea>
+      <div id="cover">
+        <div class="cover-header">
+          <a href="../js/editor/plugins/filemanager/dialog.php?ty e=0&editor=mce_0&lang=eng&fldr=&field_id=htmleditor" class="btn btn-primary btn-xs ifManager" title="Show Filemanager">
+            <i class="fa fa-files-o"></i>
+          </a>
+          <a href="#" id="resizeContainer" class="btn btn-primary btn-xs" title="Resize ACE Editor">Resize ACE Container</a>
+          <a href="#" id="resizeContainerAndEditor" class="btn btn-primary btn-xs" title="Resize ACE Editor">Resize ACE Container and ACE Editor</a>
+        </div>
+        <div id="editorContainer">
+          <div id="htmleditor"></div>
+        </div>
+      </div>
+
+      <textarea name="jak_content" class="form-control hidden" id="jak_editor"><?php if (isset($_REQUEST["jak_content"])) echo jak_edit_safe_userpost(htmlspecialchars($_REQUEST["jak_content"])); ?></textarea>
     <?php } else { ?>
-      <textarea name="jak_content" class="form-control jakEditor" id="jakEditor"
-                rows="40"><?php if (isset($_REQUEST["jak_content"])) echo jak_edit_safe_userpost($_REQUEST["jak_content"]); ?></textarea>
+      <textarea name="jak_content" class="form-control jakEditor" id="jakEditor" rows="40"><?php if (isset($_REQUEST["jak_content"])) echo jak_edit_safe_userpost($_REQUEST["jak_content"]); ?></textarea>
     <?php } ?>
   </div>
   <div class="box-footer">
     <button type="submit" name="save" class="btn btn-primary pull-right"><?php echo $tl["general"]["g20"]; ?></button>
   </div>
 </div>
+<style type="text/css">
+  #editorContainer {
+    height: 500px;
+    position: relative;
+  }
+  #htmleditor {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+  #cover.active {
+    position:fixed;
+    top:0;
+    left:0;
+    background:#f4f4f4;
+    z-index:1050;
+    width:100%;
+    height:100%;
+    padding: 40px;
+  }
+  .cover-header {
+    background:#ddd;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+</style>
+<script type="text/javascript">
+  var clicked = false;
+  var resizeFirstEditor = function (resizeEditor) {
+    var MeContainer = document.getElementById('cover');
+    var feContainer = document.getElementById('editorContainer');
+
+    MeContainer.classList.toggle("active");
+    clicked = !clicked;
+    if(resizeEditor) {
+      editor.resize();
+    }
+  };
+
+  var btn = document.getElementById('resizeContainer');
+  btn.addEventListener('click', function () {
+    resizeFirstEditor();
+  });
+  var btn = document.getElementById('resizeContainerAndEditor');
+  btn.addEventListener('click', function () {
+    resizeFirstEditor(true);
+  });
+</script>
