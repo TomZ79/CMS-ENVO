8f123d436a4c0c49446ce66589b1e8d06482e16a
bluesatkv <bluesatkv@gmail.com>
2016-10-09 11:06:15 +0200

    Section: ACP
    - redesign pro stránku 404 v ACP

--- admin/template/404.php
+++ admin/template/404.php
@@ -3,9 +3,9 @@
   <!-- Main content -->
   <section class="content">
     <div class="error-page">
-      <h2 class="headline text-yellow"> 404</h2>
+      <h2 class="headline text-warning-800"> 404</h2>
       <div class="error-content">
-        <h3><i class="fa fa-warning text-yellow"></i> <?php echo $tl["error"]["404"]; ?></h3>
+        <h3><i class="fa fa-warning text-warning-800"></i> <?php echo $tl["error"]["404"]; ?></h3>
         <p>
           We could not find the page you were looking for.
           Meanwhile, you may <a href="<?php echo BASE_URL; ?>">return to home</a>.
