dd42b500fbcea9deb2551abb17b094ca325807dd
bluesatkv <bluesatkv@gmail.com>
2016-12-19 01:07:51 +0100

    Section: ACP
    - stejný design editoru pro editor_new a editor_edit

--- admin/template/editor_new.php
+++ admin/template/editor_new.php
@@ -7,24 +7,30 @@
     </div>
   </div>
   <div class="box-body">
-    <?php if ($jkv["adv_editor"]) { ?>
-      <div id="cover">
-        <div class="cover-header">
-          <a href="../js/editor/plugins/filemanager/dialog.php?ty e=0&editor=mce_0&lang=eng&fldr=&field_id=htmleditor" class="btn btn-primary btn-xs ifManager" title="Show Filemanager">
-            <i class="fa fa-files-o"></i>
-          </a>
-          <a href="#" id="resizeContainer" class="btn btn-primary btn-xs" title="Resize ACE Editor">Resize ACE Container</a>
-          <a href="#" id="resizeContainerAndEditor" class="btn btn-primary btn-xs" title="Resize ACE Editor">Resize ACE Container and ACE Editor</a>
-        </div>
-        <div id="editorContainer">
-          <div id="htmleditor"></div>
-        </div>
-      </div>
+    <table class="table table-striped">
+      <tr>
+        <td>
+          <?php if ($jkv["adv_editor"]) { ?>
+            <div id="cover">
+              <div class="cover-header">
+                <a href="../js/editor/plugins/filemanager/dialog.php?ty e=0&editor=mce_0&lang=eng&fldr=&field_id=htmleditor" class="btn btn-primary btn-xs ifManager" title="Show Filemanager">
+                  <i class="fa fa-files-o"></i>
+                </a>
+                <a href="#" id="resizeContainer" class="btn btn-primary btn-xs" title="Resize ACE Editor">Resize ACE Container</a>
+                <a href="#" id="resizeContainerAndEditor" class="btn btn-primary btn-xs" title="Resize ACE Editor">Resize ACE Container and ACE Editor</a>
+              </div>
+              <div id="editorContainer">
+                <div id="htmleditor"></div>
+              </div>
+            </div>
 
-      <textarea name="jak_content" class="form-control hidden" id="jak_editor"><?php if (isset($_REQUEST["jak_content"])) echo jak_edit_safe_userpost(htmlspecialchars($_REQUEST["jak_content"])); ?></textarea>
-    <?php } else { ?>
-      <textarea name="jak_content" class="form-control jakEditor" id="jakEditor" rows="40"><?php if (isset($_REQUEST["jak_content"])) echo jak_edit_safe_userpost($_REQUEST["jak_content"]); ?></textarea>
-    <?php } ?>
+            <textarea name="jak_content" class="form-control hidden" id="jak_editor"><?php if (isset($_REQUEST["jak_content"])) echo jak_edit_safe_userpost(htmlspecialchars($_REQUEST["jak_content"])); ?></textarea>
+          <?php } else { ?>
+            <textarea name="jak_content" class="form-control jakEditor" id="jakEditor" rows="40"><?php if (isset($_REQUEST["jak_content"])) echo jak_edit_safe_userpost($_REQUEST["jak_content"]); ?></textarea>
+          <?php } ?>
+        </td>
+      </tr>
+    </table>
   </div>
   <div class="box-footer">
     <button type="submit" name="save" class="btn btn-primary pull-right"><?php echo $tl["general"]["g20"]; ?></button>
