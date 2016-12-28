0485414f73e23c7f64ef2c0be71139c99484d8ce
bluesatkv <bluesatkv@gmail.com>
2016-12-28 12:37:18 +0100

    Section: ACP

--- admin/template/header.php
+++ admin/template/header.php
@@ -15,6 +15,7 @@
   <link rel="stylesheet" href="js-plugins/pace/themes/teal/pace-theme-minimal.css?=<?php echo $jkv["updatetime"]; ?>" type="text/css" media="screen"/>
 
   <!-- CSS STYLE ================================================================================================ -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css" />
   <!-- General Stylesheet with custom modifications -->
   <link rel="stylesheet" href="../css/stylesheet.css?=<?php echo $jkv["updatetime"]; ?>" type="text/css" media="screen"/>
   <!-- Bootstrap -->
