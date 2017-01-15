5515299df7776bfe91670f4448fe8f8cd157177f
bluesatkv <bluesatkv@gmail.com>
2017-01-15 18:58:43 +0100

    Section: ACP
    - redesign templatesettings.php (Pokud neexistuje šablona, zobrazí se
    informace o nedostupnosti šablony.

--- admin/template/templatesettings.php
+++ admin/template/templatesettings.php
@@ -32,15 +32,15 @@ if ($page3 == "e") { ?>
 <?php } ?>
 
 <?php if (!isset($jkv["cms_tpl"])) { ?>
-	<section class="content">
-		<div class="error-page">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3 error-page">
+			<h1 class="text-warning bold"><?php echo $tl["notetemplate"]["nh"]; ?></h1>
 			<div class="error-content">
-				<h3><i class="fa fa-warning text-warning-800"></i> <?php echo $tl["notetemplate"]["nh"]; ?></h3>
-				<h4><?php echo $tl["notetemplate"]["n1"]; ?></h4>
+				<h3><i class="fa fa-warning text-warning"></i> <?php echo $tl["notetemplate"]["n1"]; ?></h3>
 				<p><?php echo $tl["notetemplate"]["n2"]; ?></p>
 			</div>
 		</div>
-	</section>
+	</div>
 <?php } else {
 
 	// Include template settings from each template if exists
