da0aa8c6fce0af6bd496e5b512fb654f9bb186a9
BluesatKV <bluesatkv@gmail.com>
2017-06-08 22:47:13 +0200

    Sekce: AKP + Plugin + Šablony

---
+++ template/porto/404.php
@@ -0,0 +1,26 @@
+<?php include_once APP_PATH . 'template/' . ENVO_TEMPLATE . '/header.php'; ?>
+
+	<!-- =========================
+	START 404 SECTION
+	============================== -->
+	<section class="feature-404-area">
+		<!-- MAIN TITLE AREA -->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<div class="feature-404-title">
+						<div class="feature-404-shadow-heading">
+							<h2><?php echo $tl["general_error"]["generror"]; ?></h2>
+						</div>
+						<h2><?php echo $tl["general_error"]["generror"]; ?></h2>
+						<p><?php echo $tl["general_error"]["generror5"]; ?></p>
+					</div>
+				</div>
+			</div>
+		</div> <!-- END TITLE -->
+	</section>
+	<!-- =========================
+  END 404 SECTION
+  ============================== -->
+
+<?php include_once APP_PATH . 'template/' . ENVO_TEMPLATE . '/footer.php'; ?>
---
+++ template/porto/config.php
@@ -0,0 +1,125 @@
+<?php
+
+/* ### CONFIG FILE ### */
+
+
+
+
+/* ### FUNCTION FILE ### */
+
+// MENU BUILDER - METRICS, parentId 0 is the root
+// Global variable - array
+$arr1 = array();
+
+/*
+ * Search in array
+ * Example: print_r(search($arr, 'name', 'cat 1'));
+ */
+function searchForId($id, $array)
+{
+  foreach ($array as $key => $val) {
+    if ($val['pagename'] === $id) {
+      return $val['catparent'];
+    }
+  }
+
+  return NULL;
+}
+
+/*
+ * Build menu
+ */
+function build_menu_porto($parent, $menu, $maincategory, $active, $mainclass, $dropdown, $dropdownclass, $dropclass, $subclass, $admin)
+{
+  // Set global variable
+  global $arr1;
+  // Search 'catparent' in array by active page name
+  $category = searchForId($active, $menu["items"]);
+
+  $html = '';
+
+  if (isset($menu['parents'][$parent])) {
+    $html .= '<ul class="' . $mainclass . '">';
+
+
+    // Loop through the array to extract element values
+    foreach ($menu['parents'][$parent] as $itemId) {
+
+      // Set class for menu with submenu if active page is in submenu
+      $classcat = (($category == $menu["items"][$itemId]["id"]) ? ' active' : ' notactive');
+
+      //IF MENU DONT HAVE SUBMENU
+      if (!isset($menu['parents'][$itemId])) {
+        $html .= '
+        <li' . (($active == $menu["items"][$itemId]["pagename"]) ? ' class="active"' : '') . '>
+        
+          <a href="' . $menu["items"][$itemId]["varname"] . '"' . (($active == $menu["items"][$itemId]["pagename"]) ? ' class="active"' : '') . '>' .
+
+            (($menu["items"][$itemId]["catimg"]) ? '<i class="' . $menu["items"][$itemId]["catimg"] . '"></i> ' : '') .
+
+             $menu["items"][$itemId]["name"] .
+
+          '</a>
+        </li>';
+      }
+
+      //IF MENU HAS SUBMENU AND MAINCATEGORY IS ACTIVE ( $maincategory = TRUE )
+      if ($maincategory) {
+        if (isset($menu['parents'][$itemId])) {
+
+          // Add 'id' of category, which have subcategory to array - only for controls
+          $arr1[] = $menu["items"][$itemId]["id"];
+
+          $html .= '
+          <li class="' . $dropdown . (($active == $menu["items"][$itemId]["pagename"]) ? ' active' : $classcat) . '">
+            
+            <a href="' . $menu["items"][$itemId]["varname"] . '"' . (($active == $menu["items"][$itemId]["pagename"]) ? ' class="active ' . $dropdownclass . $classcat . '"' : ' class="' . $dropdownclass . $classcat . '"') . '>' .
+
+            (($menu["items"][$itemId]["catimg"]) ? '<i class="' . $menu["items"][$itemId]["catimg"] . '"></i> ' : '') .
+
+            $menu["items"][$itemId]["name"] .
+
+            '</a>';
+
+          $html .= build_menu_porto($itemId, $menu, $maincategory, $active, $dropclass, $subclass, $dropdownclass, $dropclass, $subclass, $admin);
+
+          $html .= '</li>';
+        }
+      }
+
+      //IF MENU HAS SUBMENU AND MAINCATEGORY IS NOT ACTIVE ( $maincategory = FALSE )
+      if (!$maincategory) {
+        if (isset($menu['parents'][$itemId])) {
+
+          // Add 'id' of category, which have subcategory to array - only for controls
+          $arr1[] = $menu["items"][$itemId]["id"];
+
+          $html .= '
+          <li class="' . $dropdown . (($active == $menu["items"][$itemId]["pagename"]) ? ' active' : $classcat) . '">
+          
+            <a href="' . $menu["items"][$itemId]["varname"] . '"' . (($active == $menu["items"][$itemId]["pagename"]) ? ' class="active"' : '') . '>' .
+
+              (($menu["items"][$itemId]["catimg"]) ? '<i class="' . $menu["items"][$itemId]["catimg"] . '"></i> ' : '') .
+
+              $menu["items"][$itemId]["name"] .
+
+              '</a>
+          </li>';
+
+        }
+      }
+    }
+
+    if ($admin) {
+      $html .= '<li><a href="' . BASE_URL . 'admin/"> Admin KP</a></li>';
+    }
+
+    $html .= '</ul>';
+
+  }
+
+  // Return value from function
+  return $html;
+}
+
+?>
