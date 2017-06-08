137b8d8ecc5d4a86a5797859485a6f5c30b45b5f
BluesatKV <bluesatkv@gmail.com>
2017-06-08 22:48:47 +0200

    Sekce: AKP + Plugin + Šablony

---
+++ template/porto/js/porto.custom.js
@@ -0,0 +1,513 @@
+/*
+ *
+ * CMS ENVO - PORTO TEMPLATE
+ * CSS with custom modification
+ * Copyright © 2017  Bluesat.cz
+ * -----------------------------------------------------------------------
+ * Author: Thomas
+ * Email: bluesatkv@gmail.com
+ * =======================================================================
+ */
+
+/*
+ |--------------------------------------------------------------------------
+ | AJAX - CONTACT FORM SIDEBAR for widget/widget-contact-sidebar.php
+ |--------------------------------------------------------------------------
+ */
+(function ($) {
+
+  'use strict';
+
+  /*
+   Contact Form: Sidebar
+   */
+  $('#contactFormSidebar').validate({
+    invalidHandler: function (form, validator) {
+      var errors = validator.numberOfInvalids();
+      if (errors) {
+        // var firstInvalidElement = $(validator.errorList[0].element);
+        // $('html,body').scrollTop(firstInvalidElement.offset().top - 200);
+        // firstInvalidElement.focus();
+        $('html, body').animate({
+          scrollTop: $(validator.errorList[0].element).offset().top - 150
+        }, 500);
+      }
+    },
+    submitHandler: function(form) {
+
+      var $form = $(form),
+        $messageSuccess = $('#contactSuccess'),
+        $messageError = $('#contactError'),
+        $submitButton = $(this.submitButton),
+        $errorMessage = $('#mailErrorMessage');
+
+      $submitButton.button('loading');
+
+      // Ajax Submit
+      $.ajax({
+        type: 'POST',
+        url: $form.attr('action'),
+        data: {
+          name: $form.find('#name').val(),
+          email: $form.find('#email').val(),
+          phone: $form.find('#phone').val(),
+          message: $form.find('#message').val()
+        }
+      }).always(function(data, textStatus, jqXHR) {
+
+        $errorMessage.empty().hide();
+
+        $messageSuccess.removeClass('hidden');
+        $messageError.addClass('hidden');
+
+        // Reset Form
+        $form.find('.form-control')
+          .val('')
+          .blur()
+          .parent()
+          .removeClass('has-success')
+          .removeClass('has-error')
+          .find('label.error')
+          .remove();
+
+        $submitButton.button('reset');
+
+      });
+    }
+  });
+
+}).apply(this, [jQuery]);
+
+/*
+ |--------------------------------------------------------------------------
+ | BOOTSTRAP RESPONSIVE TABS
+ |--------------------------------------------------------------------------
+ */
+(function ($) {
+
+  'use strict';
+
+  $(document).on('show.bs.tab', '.nav-tabs-responsive [data-toggle="tab"]', function (e) {
+    var $target = $(e.target);
+    var $tabs = $target.closest('.nav-tabs-responsive');
+    var $current = $target.closest('li');
+    var $parent = $current.closest('li.dropdown');
+    $current = $parent.length > 0 ? $parent : $current;
+    var $next = $current.next();
+    var $prev = $current.prev();
+    var updateDropdownMenu = function ($el, position) {
+      $el
+        .find('.dropdown-menu')
+        .removeClass('pull-xs-left pull-xs-center pull-xs-right')
+        .addClass('pull-xs-' + position);
+    };
+
+    $tabs.find('>li').removeClass('next prev');
+    $prev.addClass('prev');
+    $next.addClass('next');
+
+    updateDropdownMenu($prev, 'left');
+    updateDropdownMenu($current, 'center');
+    updateDropdownMenu($next, 'right');
+  });
+
+})(jQuery);
+
+/*
+ |--------------------------------------------------------------------------
+ | FORM VALIDATION and AJAX CALL
+ |--------------------------------------------------------------------------
+ */
+
+(function ($) {
+  'use strict';
+
+  /*
+   Register Form - login.page
+   */
+  $('#registerForm').validate({
+    onkeyup: false,
+    onclick: false,
+    rules: {
+      username: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      'captcha': {
+        captcha: true
+      }
+    },
+
+    focusInvalid: false,
+    invalidHandler: function (form, validator) {
+
+      // March Height for Featured Boxes
+      $('.featured-boxes .featured-box').matchHeight();
+
+      if (!validator.numberOfInvalids())
+        return;
+
+      // Scroll animate to first error
+      $('html, body').animate({
+        scrollTop: $(validator.errorList[0].element).offset().top - 150
+      }, 500);
+
+    }
+
+  });
+
+}).apply(this, [jQuery]);
+
+(function ($) {
+  'use strict';
+
+  /*
+   Login Form - login.page
+   */
+  $('#loginForm').validate({
+    rules: {
+      loginusername: {
+        required: true
+      },
+      loginpassword: {
+        required: true
+      }
+    },
+
+    focusInvalid: false,
+    invalidHandler: function (form, validator) {
+
+      // March Height for Featured Boxes
+      $('.featured-boxes .featured-box').matchHeight();
+
+      if (!validator.numberOfInvalids())
+        return;
+
+      // Scroll animate to first error
+      $('html, body').animate({
+        scrollTop: $(validator.errorList[0].element).offset().top - 150
+      }, 500);
+
+    }
+
+  });
+
+  /*
+   Form Forget Password - login.page
+   */
+  $('#loginFormPassword').validate({
+    rules: {
+      jakE: {
+        required: true,
+        email: true
+      }
+    },
+
+    focusInvalid: false,
+    invalidHandler: function (form, validator) {
+
+      if (!validator.numberOfInvalids())
+        return;
+
+      // Scroll animate to first error
+      $('html, body').animate({
+        scrollTop: $(validator.errorList[0].element).offset().top - 150
+      }, 500);
+
+    }
+
+  });
+
+}).apply(this, [jQuery]);
+
+/*
+ |--------------------------------------------------------------------------
+ | DATA LOADING TEXT IN BUTTON
+ |--------------------------------------------------------------------------
+ */
+(function () {
+  // Button in form with validation (fix for email)
+  $("form").submit(function (e) {
+    if ($(this).valid()) {
+      var $btn = $(this).find('input[data-loading-text]');
+      $btn.button('loading');
+      return true;
+    }
+  });
+
+  // Anchor
+  $('a[data-loading-text]').click(function () {
+    $(this).button('loading');
+  });
+}());
+
+/*
+ |--------------------------------------------------------------------------
+ | NECESSARY CODE !!!
+ | bootstrap3-showmanyslideonecarousel - v1.0.0 - 2015-03-27
+ | https://github.com/rtpHarry/Bootstrap3-ShowManySlideOneCarousel
+ |
+ | Change the number of slides visible at any one time
+ |  - https://github.com/rtpHarry/Bootstrap3-ShowManySlideOneCarousel/wiki/Change-the-number-of-slides-visible-at-any-one-time
+ | About basic Bootstrap Carousel
+ |  - https://getbootstrap.com/javascript/#carousel
+ |--------------------------------------------------------------------------
+ */
+(function () {
+  $('.carousel-showmanymoveone .item').each(function () {
+    var itemToClone = $(this);
+
+    for (var i = 1; i < 4; i++) {
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-" + (i))
+        .appendTo($(this));
+    }
+  });
+
+  // setup your carousels as you normally would using JS
+  // or via data attributes according to the documentation
+  // https://getbootstrap.com/javascript/#carousel
+  $('#carouselDownload').carousel({
+    interval: 5000
+  })
+}());
+
+
+/*
+ |--------------------------------------------------------------------------
+ | NECESSARY CODE !!! - 'jakWeb' - definition
+ |--------------------------------------------------------------------------
+ */
+(function () {
+  jakWeb = {
+    jak_lang: "",
+    jak_url: "",
+    jak_url_orig: "",
+    request_uri: "",
+    jak_search_link: "",
+    envo_template: "",
+    jak_heatmap: "",
+    jak_quickedit: "",
+    jak_acp_nav: false
+  }
+})();
+
+/*
+ |--------------------------------------------------------------------------
+ | NECESSARY CODE !!! - Responsive sidebar menu - change icon on touch device
+ |--------------------------------------------------------------------------
+ */
+
+(function () {
+  "use strict";
+  var toggles = document.querySelectorAll(".c-icons");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  }
+  ;
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener("click", function (e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+
+})();
+
+/*
+ |--------------------------------------------------------------------------
+ | NECESSARY CODE !!! - AJAX SEARCH
+ |--------------------------------------------------------------------------
+ */
+
+// Ajax Search
+(function ($) {
+
+  $.fn.ajaxSearch = function (settings) {
+
+    var defaultSettings = {
+      apiURL: '',
+      resultsDiv: $('#ajaxsearchR'),
+      seo: '',
+      searchid: '',
+      msgtypeid: '',
+      msg: 'No result were found',
+      working: false,
+      append: false
+    }
+
+    $('#ajaxsearchForm').submit(function () {
+
+      /* Combining the default settings object with the supplied one */
+      sett = $.extend(defaultSettings, settings);
+
+      if (sett.working) return false;
+
+      // Input id
+      usrinput = $('#Jajaxs').val();
+
+      sett.working = true;
+      $('.loadSearchResult').fadeIn();
+
+      // Get the result
+      $.get(sett.apiURL, {
+        q: usrinput,
+        url: jakWeb.jak_url,
+        url_detail: jakWeb.jak_search_link,
+        seo: sett.seo,
+        searchid: sett.searchid,
+        msgtypeid: sett.msgtypeid
+      }, function (r) {
+
+        sett.working = false;
+        $('.loadSearchResult').fadeOut();
+
+        if (r.length) {
+
+          // If results were returned, add them to a pageContainer div,
+          // after which append them to the #resultsDiv:
+
+          var pageContainer = $('<div>').addClass('ajaxspageContainer');
+
+
+          pageContainer.append(r);
+
+          if (!sett.append) {
+            // This is executed when running a new search,
+            // instead of clicking on the More button:
+            sett.resultsDiv.empty();
+          }
+
+          pageContainer.append('<div class="clearfix"></div>').hide().appendTo(sett.resultsDiv).fadeIn('slow');
+
+          $('.hideSearchResult').fadeIn();
+          $('.hideAdvSearchResult').fadeOut();
+
+        }
+        // No result display the nothing found message
+        else {
+
+          $('.hideSearchResult').fadeOut();
+
+          // No results were found for this search.
+
+          sett.resultsDiv.empty();
+          $('<div>', {html: sett.msg}).addClass('alert bg-danger').hide().appendTo(sett.resultsDiv).fadeIn();
+        }
+      });
+
+      return false;
+
+    });
+
+    $('.hideSearchResult a').click(function (e) {
+      e.preventDefault();
+      defaultSettings.resultsDiv.empty();
+      $('.hideSearchResult').fadeOut();
+      $('.hideAdvSearchResult').fadeIn();
+      $('#Jajaxs').val('');
+    });
+  };
+
+})(jQuery);
+
+/*
+ |--------------------------------------------------------------------------
+ | NECESSARY CODE !!! - ALPHANUMERIC, NUMERIC, ALPHA VALIDATION
+ |--------------------------------------------------------------------------
+ */
+
+(function ($) {
+
+  $.fn.alphanumeric = function (p) {
+
+    p = $.extend({
+      ichars: "öäüéàèô£†Ω°¡øπœ∑€®¢æ§¨!@#$%^&*()+=[]\\\';,/{}|:<>?~`. ",
+      nchars: "",
+      allow: ""
+    }, p);
+
+    return this.each
+    (
+      function () {
+
+        if (p.nocaps) p.nchars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if (p.allcaps) p.nchars += "abcdefghijklmnopqrstuvwxyz";
+
+        s = p.allow.split('');
+        for (i = 0; i < s.length; i++) if (p.ichars.indexOf(s[i]) != -1) s[i] = "\\" + s[i];
+        p.allow = s.join('|');
+
+        var reg = new RegExp(p.allow, 'gi');
+        var ch = p.ichars + p.nchars;
+        ch = ch.replace(reg, '');
+
+        $(this).keypress
+        (
+          function (e) {
+
+            if (!e.charCode) k = String.fromCharCode(e.which);
+            else k = String.fromCharCode(e.charCode);
+
+            if (ch.indexOf(k) != -1) e.preventDefault();
+            if (e.ctrlKey && k == 'v') e.preventDefault();
+
+          }
+        );
+
+        $(this).bind('contextmenu', function () {
+          return false
+        });
+
+      }
+    );
+
+  };
+
+  $.fn.numeric = function (p) {
+
+    var az = "abcdefghijklmnopqrstuvwxyz";
+    az += az.toUpperCase();
+
+    p = $.extend({
+      nchars: az
+    }, p);
+
+    return this.each(function () {
+        $(this).alphanumeric(p);
+      }
+    );
+
+  };
+
+  $.fn.alpha = function (p) {
+
+    var nm = "1234567890";
+
+    p = $.extend({
+      nchars: nm
+    }, p);
+
+    return this.each(function () {
+        $(this).alphanumeric(p);
+      }
+    );
+
+  };
+
+})(jQuery);
+
---
+++ template/porto/js/porto.templatesettings.php
@@ -0,0 +1,59 @@
+<script type="text/javascript">
+
+  /* ACE Editor
+   ========================================= */
+  if ($('#htmleditor2').length) {
+    var htmlefACE2 = ace.edit("htmleditor2");
+    htmlefACE2.setTheme("ace/theme/<?php echo $jkv["acetheme"]; ?>"); // Theme chrome, monokai
+    htmlefACE2.session.setUseWrapMode(true);
+    htmlefACE2.session.setWrapLimitRange(<?php echo $jkv["acewraplimit"] . ',' . $jkv["acewraplimit"]; ?>);
+    htmlefACE2.setOptions({
+      // session options
+      mode: "ace/mode/<?php echo $acemode2;?>",
+      tabSize: <?php echo $jkv["acetabSize"]; ?>,
+      useSoftTabs: true,
+      highlightActiveLine: <?php echo $jkv["aceactiveline"]; ?>,
+      // renderer options
+      showInvisibles: <?php echo $jkv["aceinvisible"]; ?>,
+      showGutter: <?php echo $jkv["acegutter"]; ?>,
+    });
+    // This is to remove following warning message on console:
+    // Automatically scrolling cursor into view after selection change this will be disabled in the next version
+    // set editor.$blockScrolling = Infinity to disable this message
+    htmlefACE2.$blockScrolling = Infinity;
+
+    texthtmlef = $("#jak_filecontent2").val();
+    htmlefACE2.session.setValue(texthtmlef);
+  }
+
+  if ($('#htmleditor3').length) {
+    var htmlefACE3 = ace.edit("htmleditor3");
+    htmlefACE3.setTheme("ace/theme/<?php echo $jkv["acetheme"]; ?>"); // Theme chrome, monokai
+    htmlefACE3.session.setUseWrapMode(true);
+    htmlefACE3.session.setWrapLimitRange(<?php echo $jkv["acewraplimit"] . ',' . $jkv["acewraplimit"]; ?>);
+    htmlefACE3.setOptions({
+      // session options
+      mode: "ace/mode/<?php echo $acemode3;?>",
+      tabSize: <?php echo $jkv["acetabSize"]; ?>,
+      useSoftTabs: true,
+      highlightActiveLine: <?php echo $jkv["aceactiveline"]; ?>,
+      // renderer options
+      showInvisibles: <?php echo $jkv["aceinvisible"]; ?>,
+      showGutter: <?php echo $jkv["acegutter"]; ?>,
+    });
+    // This is to remove following warning message on console:
+    // Automatically scrolling cursor into view after selection change this will be disabled in the next version
+    // set editor.$blockScrolling = Infinity to disable this message
+    htmlefACE3.$blockScrolling = Infinity;
+
+    texthtmlef = $("#footerblocktext1").val();
+    htmlefACE3.session.setValue(texthtmlef);
+  }
+
+  /* Submit Form
+   ========================================= */
+  $('form').submit(function () {
+    $("#jak_filecontent2").val(htmlefACE2.getValue());
+    $("#footerblocktext1").val(htmlefACE3.getValue());
+  });
+</script>
