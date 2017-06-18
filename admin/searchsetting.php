7b2e08a77c5e397f7678bdf79c48b6f8cf79e103
BluesatKV <bluesatkv@gmail.com>
2017-06-18 13:54:20 +0200

    AKP - Nastavení Vyhledávání
    Odstranění speciální proměnných. Jsou zbytečné. Hodnoty máme načtené buď
    v globálních hodnotách ' $jkv ' nebo ' $JAK_SETTING_VAL '

--- admin/searchsetting.php
+++ admin/searchsetting.php
@@ -175,10 +175,6 @@ if ($hookpagei) {
   }
 }
 
-// Get the special vars for multi language support
-$JAK_FORM_DATA["title"]   = $jkv["searchtitle"];
-$JAK_FORM_DATA["content"] = $jkv["searchdesc"];
-
 // EN: Title and Description
 // CZ: Titulek a Popis
 $SECTION_TITLE = $tl["ss_sec_title"]["sst"];
--- admin/template/searchsetting.php
+++ admin/template/searchsetting.php
@@ -104,7 +104,7 @@ if ($errors) { ?>
 
                           <?php
                           // Add Html Element -> addInput (Arguments: type, name, value, id, class, optional assoc. array)
-                          echo $Html->addInput('text', 'jak_title', $JAK_FORM_DATA["title"], '', 'form-control');
+                          echo $Html->addInput('text', 'jak_title', $JAK_SETTING_VAL["searchtitle"], '', 'form-control');
                           ?>
 
                         </div>
@@ -123,7 +123,7 @@ if ($errors) { ?>
 
                         <?php
                         // Add Html Element -> addTextarea (Arguments: name, value, rows, cols, optional assoc. array)
-                        echo $Html->addTextarea('jak_lcontent', jak_edit_safe_userpost($JAK_FORM_DATA["content"]), '4', '', array('id' => 'jakEditor', 'class' => 'jakEditorLight form-control'));
+                        echo $Html->addTextarea('jak_lcontent', jak_edit_safe_userpost($JAK_SETTING_VAL["searchdesc"]), '4', '', array('id' => 'jakEditor', 'class' => 'jakEditorLight form-control'));
                         ?>
 
                       </div>
