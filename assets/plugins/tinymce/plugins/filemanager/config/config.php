060914688cf56ec7121f85d9d1bee2525b77fbba
BluesatKV <bluesatkv@gmail.com>
2017-06-18 18:40:05 +0200

    AKP - Fix typo

--- assets/plugins/tinymce/plugins/filemanager/config/config.php
+++ assets/plugins/tinymce/plugins/filemanager/config/config.php
@@ -5,8 +5,6 @@ require_once '../../../../../config_tinymce.php';
 
 if (session_id() == '') session_start();
 
-if (JAK_ADMINACCESS) $_SESSION['RF']["verify"] = "JAKfilemanager";
-
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
 mb_http_input('UTF-8');
@@ -14,8 +12,6 @@ mb_language('uni');
 mb_regex_encoding('UTF-8');
 ob_start('mb_output_handler');
 date_default_timezone_set('Europe/Rome');
-
-if (!isset($_SESSION['RF']["verify"]) || $_SESSION['RF']["verify"] != "JAKfilemanager") die('Nothing to see here.');
 
 /*
 |--------------------------------------------------------------------------
