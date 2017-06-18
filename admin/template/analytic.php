7f9657a8c30e55c1b63190eb2a0c79fab925c744
BluesatKV <bluesatkv@gmail.com>
2017-06-18 18:39:38 +0200

    AKP - Piwik
    Odstranění iframu na Piwik

--- admin/template/analytic.php
+++ admin/template/analytic.php
@@ -1,10 +1,6 @@
 <div class="col-md-6">
-  <div id="widgetIframe">
-    <iframe width="100%" height="380" src="https://analytics.bluesat.cz/index.php?module=Widgetize&action=iframe&widget=1&moduleToWidgetize=SEO&actionToWidgetize=getRank&idSite=1&period=day&date=yesterday&disableLink=1&widget=1" scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe>
-  </div>
+
 </div>
 <div class="col-md-6">
-  <div id="widgetIframe">
-    <iframe width="100%" height="350" src="https://analytics.bluesat.cz/index.php?module=Widgetize&action=iframe&forceView=1&viewDataTable=graphEvolution&widget=1&moduleToWidgetize=VisitsSummary&actionToWidgetize=getEvolutionGraph&idSite=1&period=day&date=yesterday&disableLink=1&widget=1" scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe>
-  </div>
+
 </div>
