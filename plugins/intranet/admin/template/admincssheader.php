4aedf2cb16a533180b6e780298c6c7f92c31ddfa
BluesatKV <bluesatkv@gmail.com>
2017-08-11 10:56:31 +0200

    Plugin Intranet
    Přidána možnost vkládání css stylů pro administrační rozhraní do
    vlastního externího souboru.

---
+++ plugins/intranet/admin/template/admincssheader.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * PLUGIN INTRANET - POPIS SOUBORU admincssheader.php
+ * ----------------------------------------------
+ *
+ * Soubor slouží pro vložení css stylu souboru 'plugins/intranet/admin/css/style.css' do záhlaví webu - admin
+ *
+ */
+?>
+
+<link rel="stylesheet" href="<?php echo BASE_URL_ORIG; ?>plugins/intranet/admin/css/style.css" type="text/css"/>
