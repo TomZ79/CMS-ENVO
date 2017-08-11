ac3c206eacc06de37a570bead65a1429757be0cb
BluesatKV <bluesatkv@gmail.com>
2017-08-11 10:54:33 +0200

    Plugin XML SEO
    Fix typo

--- plugins/xml_seo/admin/template/admincssheader.php
+++ plugins/xml_seo/admin/template/admincssheader.php
@@ -1,1 +1,11 @@
+<?php
+/*
+ * PLUGIN XML SEO - POPIS SOUBORU admincssheader.php
+ * ----------------------------------------------
+ *
+ * Soubor slouží pro vložení css stylu souboru 'plugins/xml_seo/admin/css/style.css' do záhlaví webu - admin
+ *
+ */
+?>
+
 <link rel="stylesheet" href="<?php echo BASE_URL_ORIG; ?>plugins/xml_seo/admin/css/style.css" type="text/css"/>
