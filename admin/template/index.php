86b3d96946b5301e1ea73c8f5a24b0e59712685a
BluesatKV <bluesatkv@gmail.com>
2017-12-25 15:18:14 +0100

    Update Pages 3.0
    Update html struktury Dashboard stránky.

--- admin/template/index.php
+++ admin/template/index.php
@@ -86,24 +86,24 @@
 
   <!-- Content -->
   <ul class="nav nav-tabs nav-tabs-responsive nav-tabs-fillup" role="tablist">
-    <li role="presentation" class="active">
-      <a href="#cmsPage1" id="cmsPage1-tab" role="tab" data-toggle="tab" aria-controls="cmsPage1" aria-expanded="true">
+    <li class="nav-item">
+      <a href="#" class="active" data-toggle="tab" data-target="#cmsPage1" role="tab">
         <span class="text"><?php echo $tl["dashb_section_tab"]["dashbtab"]; ?></span>
       </a>
     </li>
-    <li role="presentation" class="next">
-      <a href="#cmsPage2" id="cmsPage2-tab" role="tab" data-toggle="tab" aria-controls="cmsPage2" aria-expanded="true">
+    <li class="nav-item next">
+      <a href="#" class="" data-toggle="tab" data-target="#cmsPage2" role="tab">
         <span class="text"><?php echo $tl["dashb_section_tab"]["dashbtab1"]; ?></span>
       </a>
     </li>
-    <li role="presentation">
-      <a href="#cmsPage3" id="cmsPage3-tab" role="tab" data-toggle="tab" aria-controls="cmsPage3" aria-expanded="true">
+    <li class="nav-item">
+      <a href="#" class="" data-toggle="tab" data-target="#cmsPage3" role="tab">
         <span class="text"><?php echo $tl["dashb_section_tab"]["dashbtab2"]; ?></span>
       </a>
     </li>
     <?php if (isset($ENVO_HOOK_ADMIN_INDEX)) { ?>
-      <li role="presentation">
-        <a href="#cmsPage4" role="tab" id="cmsPage4-tab" data-toggle="tab" aria-controls="cmsPage4">
+      <li class="nav-item">
+        <a href="#" class="" data-toggle="tab" data-target="#cmsPage4" role="tab">
           <span class="text"><?php echo $tl["dashb_section_tab"]["dashbtab3"]; ?></span>
         </a>
       </li>
@@ -111,7 +111,7 @@
   </ul>
 
   <div class="tab-content">
-    <div role="tabpanel" class="tab-pane fade in active" id="cmsPage1" aria-labelledby="cmsPage1-tab">
+    <div class="tab-pane active" id="cmsPage1" role="tabpanel">
       <div class="row">
         <div class="col-md-6">
           <div class="box box-success">
@@ -157,7 +157,7 @@
         </div>
       </div>
     </div>
-    <div role="tabpanel" class="tab-pane fade" id="cmsPage2" aria-labelledby="cmsPage2-tab">
+    <div class="tab-pane" id="cmsPage2" role="tabpanel">
       <div class="row">
         <div class="col-md-12">
           <div class="box box-success">
@@ -181,7 +181,7 @@
         </div>
       </div>
     </div>
-    <div role="tabpanel" class="tab-pane fade" id="cmsPage3" aria-labelledby="cmsPage3-tab">
+    <div class="tab-pane" id="cmsPage3" role="tabpanel">
       <div class="row">
         <div class="col-md-12">
           <div class="box box-success">
@@ -214,7 +214,7 @@
       </div>
     </div>
     <?php if (isset($ENVO_HOOK_ADMIN_INDEX)) { ?>
-      <div role="tabpanel" class="tab-pane fade" id="cmsPage4" aria-labelledby="cmsPage4-tab">
+      <div class="tab-pane" id="cmsPage4" role="tabpanel">
         <div class="row">
           <div class="col-md-12">
             <?php
